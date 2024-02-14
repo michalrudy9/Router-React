7046de94d0ffca8d2a310b18d278cb75efc46f57
michalrudy9 <michallrudy9@gmail.com>
2024-02-14 21:04:22 +0100

    Added router for application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />}/>
+//     <Route path="/products" element={<Products />}/>
+//   </Route>
+// );
+
+// const routerFromDefinition = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
