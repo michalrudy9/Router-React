dd8f49dbd6d7b0c400924e4ff7b45be932feb972
michalrudy9 <michallrudy9@gmail.com>
2024-02-15 20:41:23 +0100

    Added new project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import RootLayout from "./pages/RootLayout";
-import Error from "./pages/Error";
-import ProductDetail from "./pages/ProductDetail";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/products", element: <Products /> },
-      { path: "/products/:productId", element: <ProductDetail /> },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
