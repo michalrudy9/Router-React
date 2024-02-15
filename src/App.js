fcc5835bef7f57930cf63c3e283f8e6373bfc160
michalrudy9 <michallrudy9@gmail.com>
2024-02-15 16:41:54 +0100

    Added error page when not find page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/RootLayout";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       { path: "/products", element: <Products /> },
