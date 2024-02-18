76931fc62480f318afdef238ee059d3c16d9af2a
michalrudy9 <michallrudy9@gmail.com>
2024-02-18 20:01:59 +0100

    Added loading events indicator with defer

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,20 +1,34 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
-  return <EventsList events={events} />;
+  const { events } = useLoaderData();
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function eventsLoader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     return json({ message: "Could not fetch events!" }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function eventsLoader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
