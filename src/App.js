d542b9478066586f797fa3d9f876dfce184aeb35
michalrudy9 <michallrudy9@gmail.com>
2024-02-19 19:50:33 +0100

    Added authentication feature

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import { manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -53,6 +54,7 @@ const router = createBrowserRouter([
           },
         ],
       },
+      { path: "auth", element: <AuthenticationPage /> },
       {
         path: "newsletter",
         element: <NewsletterPage />,
