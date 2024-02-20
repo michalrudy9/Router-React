074501ecfe87ccba006baeff3102f66514338111
michalrudy9 <michallrudy9@gmail.com>
2024-02-20 15:15:28 +0100

    Added user logout

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, {
   action as authAtcion,
 } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
   {
@@ -62,6 +63,7 @@ const router = createBrowserRouter([
         element: <NewsletterPage />,
         action: newsletterAction,
       },
+      { path: "logout", action: logoutAction },
     ],
   },
 ]);
